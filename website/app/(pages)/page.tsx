4dca9204ed247ab037ecb87128e99360407220b5
Hugo Lageneste <hugo.lageneste@pm.me>
2024-10-22 14:38:23 +0200

    Initialise task home page

--- website/app/(pages)/page.tsx
+++ website/app/(pages)/page.tsx
@@ -1,9 +1,33 @@
 'use client'
 
+import { Flex, Heading, chakra } from '@chakra-ui/react'
+
 const Page = () => {
-  return <>
-    Hello
-  </>
+  return <Flex
+    w="100vw" h="100vh"
+    gap="20px"
+    p="40px"
+  >
+    <Flex 
+      flex="1" h="100%"
+    >
+
+    </Flex>
+
+    <Flex 
+      flex="2" h="100%"
+    >
+      <Heading size="2xl">
+        Today <chakra.span fontSize="0.6em" color="grey">(5)</chakra.span>
+      </Heading>
+    </Flex>
+
+    <Flex 
+      flex="1" h="100%"
+    >
+
+    </Flex>
+  </Flex>
 }
 
 export default Page
--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,15 +1,14 @@
-import { Helvetica, theme } from '@/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode;}>) => {
   return (
-    <html lang="en" className={Helvetica.variable}>
+    <html lang="en">
       <head>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"></meta>
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
       </head>
       <body>
-        <ChakraProvider theme={theme}>
+        <ChakraProvider>
           {children}
         </ChakraProvider>
       </body>
